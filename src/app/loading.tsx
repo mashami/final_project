b1714eb52b7d87fe55b03e34f380d3b500a4fb83
Maker Studios <mashamipaccy04@gmail.com>
2023-08-12 12:56:52 +0200

    feat: authentication back end

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,8 +2,9 @@ import { Loader } from "@/components/Loader"
 
 const Loading = () => {
   return (
-    <div className="h-screen grid place-items-center">
+    <div className="h-screen w-screen flex justify-center items-center space-x-4 ">
       <Loader />
+      <p>Loading...</p>
     </div>
   )
 }
